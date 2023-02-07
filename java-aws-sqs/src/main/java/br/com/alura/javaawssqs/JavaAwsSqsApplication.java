c105d8e0e9d0f990915010744390d11fafd36b7d
Gusta <thegustavosantos@outlook.com>
2023-02-06 22:54:56 -0300

    starting

---
+++ java-aws-sqs/src/main/java/br/com/alura/javaawssqs/JavaAwsSqsApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.javaawssqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAwsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAwsSqsApplication.class, args);
+	}
+
+}
---
+++ java-aws-sqs/src/test/java/br/com/alura/javaawssqs/JavaAwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.javaawssqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAwsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
